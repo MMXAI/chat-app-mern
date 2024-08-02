7884ff423b6480214812f0bef263974c126fa929
MohammadX <shahrabadi.msv@gmail.com>
2024-08-03 02:35:54 +0330

    Created User Schema and User Model

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-// App Routes
-app.get("/", (req, res) => {
-    // root route http://localhost:PORT
-    res.send("[+] Server is Up and Running");
-});
+dotenv.config();
+
+app.use(express.json());    // to parse the incoming requets with JSON payloads (from req.body)
 
 app.use("/api/auth", authRoutes);
+
+// App Routes
+// app.get("/", (req, res) => {
+//     // root route http://localhost:PORT
+//     res.send("[+] Server is Up and Running");
+// });
+
 
 app.listen(PORT, () => {
     connectToMongoDB();
