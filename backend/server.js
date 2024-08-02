11abd5353a704fae75c87de3d55926ee420aa1c4
MohammadX <shahrabadi.msv@gmail.com>
2024-08-02 16:25:24 +0330

    Setup env variables (.env) & enabled package module import

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
-const express = require("express");
-
+import express from "express";
+import dotenv from "dotenv";
 const app = express();
 
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+
 app.get("/", (req, res) => {
-    // root route http://localhost:5000
+    // root route http://localhost:PORT
     res.send("[+] Server is Up and Running");
 });
 
-app.listen(5000, () => console.log("[+] Server Running on port 5000"));
+app.listen(PORT, () => console.log(`[+] Server Running on port ${PORT}`));
