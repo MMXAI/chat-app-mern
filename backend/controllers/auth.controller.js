2221fb082748bf778ee72330f4cb41cd636579f2
MohammadX <shahrabadi.msv@gmail.com>
2024-08-03 01:43:09 +0330

    Updating Routes & Controllers

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,10 @@
-export const signup = (req, res) => {
-    console.log("signup User");
+export const signup = async (req, res) => {
+    try {
+        const {fullName,username,password,confirmPassword,gender} = req.body;
+        // Now we need to setup our database first
+    } catch (error) {
+        
+    }
 }
 
 export const login = (req, res) => {
