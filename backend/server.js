305eb27a3b2784fc121e4264aeccae96d7f2ff30
MohammadX <shahrabadi.msv@gmail.com>
2024-08-02 16:47:55 +0330

    Setting Routes

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+
 const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
+// App Routes
 app.get("/", (req, res) => {
     // root route http://localhost:PORT
     res.send("[+] Server is Up and Running");
 });
+
+app.use("/api/auth", authRoutes);
+
 
 app.listen(PORT, () => console.log(`[+] Server Running on port ${PORT}`));
