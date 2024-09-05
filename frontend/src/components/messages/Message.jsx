aca1cdfab990211f6a8ebe79d470a7a0f6b48b2b
MohammadX <shahrabadi.msv@gmail.com>
2024-09-06 03:04:06 +0330

    Added useGetMessages Hook and wired it up to the Messages Component
    "extractTime.js" is for converting MongoDB createdAt field format
    to a more readable format.
    "MessageSkeleton.jsx" is a dummy message template that we use
    when the user is waiting for the actual messages to load.

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,18 +1,32 @@
-function Message() {
+import { useAuthContext } from "../../context/AuthContext";
+import { extractTime } from "../../utils/extractTime";
+import useConversation from "../../zustand/useConversation";
+
+const Message = ({ message }) => {
+  const { authUser } = useAuthContext();
+  const { selectedConversation } = useConversation();
+  const fromMe = message.senderId === authUser._id;
+  const formattedTime = extractTime(message.createdAt);
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
+  const profilePic = fromMe
+    ? authUser.profilePic
+    : selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? "bg-orange-600" : "bg-purple-600";
   return (
-    <div className="chat chat-end">
+    <div className={`chat ${chatClassName}`}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
-          <img
-            src="https://avatar.iran.liara.run/public/36"
-            alt="Tailwind CSS chat bubble component"
-          />
+          <img src={profilePic} alt="Tailwind CSS chat bubble component" />
         </div>
       </div>
-      <div className="chat-bubble text-white bg-blue-500">Hi! What is up?</div>
-      <div className="chat-footer text-white opacity-60 text-xs flex gap-1 items-center">12:42</div>
+      <div className={`chat-bubble text-white ${bubbleBgColor} pb-2`}>
+        {message.message}
+      </div>
+      <div className="chat-footer text-white opacity-60 text-xs flex gap-1 items-center pb-2">
+        {formattedTime}
+      </div>
     </div>
   );
-}
+};
 
 export default Message;
